0cbb0c71fbd6935f840d65896208dcce956e4542
awesomeakokayo <awesomeakokayo@gmail.com>
2025-08-03 06:34:20 -0700

    new file:   index.css 	new file:   index.html 	new file:   index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+homeScore = document.getElementById("hscore")
+hcount = 0
+
+function haddone() {
+    hcount = hcount += 1
+    homeScore.innerText = hcount
+    console.log(homeScore)
+}
+
+function haddtwo() {
+    hcount = hcount += 2
+    homeScore.innerText = hcount
+    console.log(homeScore)
+}
+
+function haddthree() {
+    hcount = hcount += 3
+    homeScore.innerText = hcount
+    console.log(homeScore)
+}
+
+guestScore = document.getElementById("gscore")
+gcount = 0
+
+function gaddone() {
+    gcount = gcount += 1
+    guestScore.innerText = gcount
+    console.log(gcount)
+}
+
+function gaddtwo() {
+    gcount = gcount += 2
+    guestScore.innerText = gcount
+    console.log(gcount)
+}
+
+function gaddthree() {
+    gcount = gcount += 3
+    guestScore.innerText = gcount
+    console.log(gcount)
+}
+
+leadTeam = document.getElementById("lead-team")
+
+function lead() {
+    if (hcount > gcount) {
+        leadTeam.innerText = "Home Team is leading"
+    }
+    else if (hcount == gcount) {
+        leadTeam.innerText = "It is a Draw"
+    }
+    else {
+        leadTeam.innerText = "Guest Team is leading"
+    }
+}
